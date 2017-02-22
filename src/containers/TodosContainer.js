1d629eb9eed26fb8d6ea5f34715da1d032575a51
zZissou <kyle.edward.russell@gmail.com>
2017-02-22 13:14:23 -0800

    set up initial components, config, containers, and routes

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <div className="todosContainer">
+        <h2>This is the todos container</h2>
+      </div>
+    )
+  }
+}
+
+export default TodosContainer
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import {Router, browserHistory} from 'react-router'
+import routes from './config/routes.js'
 
 ReactDOM.render(
-  <App />,
+  <Router routes={routes} history={browserHistory}/>,
   document.getElementById('root')
 );
